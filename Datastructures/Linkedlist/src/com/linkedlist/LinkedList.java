4cadb7c35583c39c220f0b1ee9d42b54504dfd3e
daniyaniazi <farihadania@hotmail.com>
2022-09-21 03:03:57 -0700

    add linkedlist

---
+++ Datastructures/Linkedlist/src/com/linkedlist/LinkedList.java
@@ -0,0 +1,113 @@
+package com.linkedlist;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    private class Node  {
+//        no need of setters
+        private  int value;
+        private Node next;
+
+        public  Node(int value){
+            this.value=value;
+        }
+    }
+    private int size;
+
+    private Node first;
+    private Node last;
+
+    private boolean isEmpty(){
+        return first == null;
+    }
+
+    // addFirst
+    public  void    addFirst(int item){
+        Node node = new Node(item);
+                if(isEmpty()){
+                  first=last=node;
+                }else{
+                    node.next= first;
+                    first = node;
+                }
+                size++;
+    }
+    //addLast
+    public  void  addLast(int item){
+        Node node = new Node(item);
+
+        if(isEmpty()){
+            first = last = node;
+        }else{
+            last.next = node;
+            last = node;
+        }
+        size++;
+    }
+    //deleteFirst
+    public  void removeFirst(){
+        if(isEmpty()) throw new NoSuchElementException();
+        if(first==last){
+            first = last = null;
+        }
+        else{
+        Node second=first.next;
+        first.next=null;
+        first = second;
+        }
+        size--;
+    }
+    //deleteLast
+    public void  removeLast(){
+        if(isEmpty()) throw new NoSuchElementException();
+        if(first==last){
+            first = last = null;
+        }else { Node prev = getPrevoius(last);
+            last=prev;
+            last.next=null;
+        }
+       size--;
+    }
+    private  Node getPrevoius(Node node){
+        Node current = first;
+        while (current!=null){
+            if (current.next==node) return current;
+            current = current.next;
+
+        }
+        return null;
+    }
+    //contains
+    //indexOf
+    public  int indexOf(int item){
+        int index =0;
+        Node current = first;
+        while (current!=null){
+            if(current.value==item)return  index;
+                current=current.next;
+                index=index+1;
+        }
+        return  -1;
+    }
+    public int[] toArray(){
+        int[] array = new int[size];
+
+        Node current= first;
+        int index=0;
+        while(current!=null){
+            array[index++]=current.value;
+            current=current.next;
+        }
+        return array;
+    }
+public int size(){
+        return  size;
+
+}
+    public  boolean contains(int item){
+        return  indexOf(item)!= -1;
+
+    }
+
+}
---
+++ Datastructures/Linkedlist/src/com/linkedlist/Main.java
@@ -0,0 +1,25 @@
+package com.linkedlist;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        System.out.println(list.size());
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(25);
+        list.addLast(36);
+        System.out.println(list.size());
+        System.out.println(list.indexOf(200));
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list.contains(10));
+        System.out.println(list.size());
+        int[] array = list.toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
