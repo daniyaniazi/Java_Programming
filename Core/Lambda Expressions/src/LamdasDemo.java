ec7a20cba63027f1cd193203a04d33b5b7dfdc67
daniyaniazi <farihadania@hotmail.com>
2022-10-07 05:30:34 -0700

    anonymous inner class

--- Core/Lambda Expressions/src/LamdasDemo.java
+++ Core/Lambda Expressions/src/LamdasDemo.java
@@ -1,6 +1,14 @@
 public class LamdasDemo {
     public  static  void show(){
         greet(new ConsolePrinter());
+
+        // Anonymous inner class inside a method
+        greet(new IPrinter() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
 
     public  static  void greet(IPrinter printer){
