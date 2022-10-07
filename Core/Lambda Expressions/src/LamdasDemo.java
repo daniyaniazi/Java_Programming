c045a0ea68e0169164dac5becb62ef52e04a02ce
daniyaniazii <daniya.niazi@techlogix.com>
2022-10-07 12:20:50 -0700

    lambda exp

--- Core/Lambda Expressions/src/LamdasDemo.java
+++ Core/Lambda Expressions/src/LamdasDemo.java
@@ -1,14 +1,29 @@
 public class LamdasDemo {
+    public void print(String message){}
     public  static  void show(){
+        String localVar ="_";
         greet(new ConsolePrinter());
 
         // Anonymous inner class inside a method
+        // this refrences to current instance of anonymous inner class
+        // can have state
         greet(new IPrinter() {
             @Override
             public void print(String message) {
                 System.out.println(message);
             }
         });
+
+        // lambda expression
+        //can access local vars , instance field
+        // this reference to enclosing obj
+        // no state it's just an anonymous func
+        greet(message-> System.out.println(localVar+message));
+        greet(System.out::println); //class/object::method
+
+
+        LamdasDemo demo = new LamdasDemo();
+        greet(demo::print);
     }
 
     public  static  void greet(IPrinter printer){
