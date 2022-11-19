b5fa3f77e1918ea5965aba99b96c41b3e7d98809
daniyaniazi <farihadania@hotmail.com>
2022-11-20 00:52:12 +0500

    final refactor

--- ProblemSolving/Stack/Easy_02_BalancedExpression/src/ValidExpression.java
+++ ProblemSolving/Stack/Easy_02_BalancedExpression/src/ValidExpression.java
@@ -1,19 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class ValidExpression {
 
+    private final List leftBrackets = Arrays.asList('(' ,'<' ,'[','{' );
+    private final List rightBrackets = Arrays.asList(')' ,'>' ,']','}' );
+
     private  boolean isLeftBracket(Character ch){
-       return ch == '(' || ch == '<' || ch=='[' || ch=='{' ;
+        return leftBrackets.contains(ch) ;
     }
     private  boolean isRightBracket(Character ch){
-        return ch == ')'|| ch == '>' || ch==']' || ch=='}';
+        return rightBrackets.contains(ch) ;
     }
 
     private boolean bracketsMatch(char ch , char top){
-        return (ch == ')' && top !='(')
-                || (ch == '>' && top !='<')
-                || (ch == ']' && top !='[')
-                || (ch == '}' && top !='{');
+    return   leftBrackets.indexOf(top) == rightBrackets.indexOf(ch);
+//        return (ch == ')' && top !='(')
+//                || (ch == '>' && top !='<')
+//                || (ch == ']' && top !='[')
+//                || (ch == '}' && top !='{');
     }
     public  boolean ValidParanthesis(String input){
         Stack<Character> stack= new Stack<>();
@@ -28,7 +35,7 @@ public class ValidExpression {
                 if (stack.empty()) return  false;
                 Character top =stack.pop();
 
-                if(bracketsMatch(ch,top)){
+                if(!bracketsMatch(ch,top)){
                     return false;
                 }
             }
