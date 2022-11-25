85c7d2921b0ab79d35c6ccf1bf1be998f345ae52
daniyaniazii <daniya.niazi@techlogix.com>
2022-11-26 02:26:57 +0500

    sets

--- Datastructures/HashTables/src/Main.java
+++ Datastructures/HashTables/src/Main.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer,String> map = new HashMap<>();
-        map.put(1,"Mosh");
-        map.put(2,"Jill");
-        map.put(3,"Mike");
-        map.put(1,"Jhon"); //overwrite
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Mosh");
+        map.put(2, "Jill");
+        map.put(3, "Mike");
+        map.put(1, "Jhon"); //overwrite
         map.remove(2);
         String mapValue = map.get(1);
         System.out.println(mapValue);
@@ -15,8 +15,17 @@ public class Main {
         System.out.println(map.containsValue("Mike")); //O(n)
 
         // for (var item : map); //error - for loops canot run on hashmap
-        for (Object item: map.keySet()){
+        for (Object item : map.keySet()) {
             System.out.println(item); //O(n)
         }
+
+//       SETS
+        Set<Integer> set = new HashSet<>();
+        int[] numbers = {1, 4, 5, 5, 3, 24, 3, 4, 3, 6};
+
+        for (int num:numbers) {
+        set.add(num);
+        }
+        System.out.println(set);
     }
 }
