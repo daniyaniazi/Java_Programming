48ed2a37f45fc93dcad621ccbb60a2fc52a98f7d
daniyaniazii <daniya.niazi@techlogix.com>
2022-12-25 19:40:57 +0500

    Daily Challange brute force

---
+++ ProblemSolving/Arrays/_03_Easy_LongestSubsquenceSum/src/BruteForce.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+
+        int sum=0;
+        int count=0;
+        int[] ans= new int[queries.length];
+        Arrays.sort(nums);
+        for(int i=0;i<queries.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(sum+nums[j]>queries[i]){
+                    continue;
+                }
+                else{
+                    sum=sum+nums[j];
+                    count++;
+                }
+            }
+            ans[i]=count;
+            sum=0;
+            count=0;
+        }
+        return ans;
+    }
+}
