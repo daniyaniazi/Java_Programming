8fddfae203d1c3496891ed1ed9ab125c643f43fe
daniyaniazi <farihadania@hotmail.com>
2022-12-27 09:24:58 +0500

    Lowest common ancestor with dfs

---
+++ ProblemSolving/BinaryTrees/DFS/_01_Medium_LowestCommonAncestor/src/Main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        int curr = root.val;
+
+        if (p.val > curr && q.val > curr) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        if (p.val < curr && q.val < curr) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        return root;
+    }
+}
