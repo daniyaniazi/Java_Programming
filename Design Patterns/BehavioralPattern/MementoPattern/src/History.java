99a4d19a4ae33daa75758871ac0852a47f41dad4
daniyaniazi <farihadania@hotmail.com>
2023-02-09 12:12:24 +0500

    Implement memento Pattern

---
+++ Design Patterns/BehavioralPattern/MementoPattern/src/History.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class History {
+
+    private List<EditorState> states= new ArrayList<>();
+
+    public  void  push(EditorState state){
+        states.add(state);
+    }
+
+    public  EditorState pop(){
+        EditorState lastState = states.get(states.size() -1);
+        states.remove(lastState);
+        return lastState;
+    }
+
+}
