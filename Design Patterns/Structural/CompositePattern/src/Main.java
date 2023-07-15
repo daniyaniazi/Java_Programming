9dd7183517da43315f9282d3f95e744c6d5823c3
daniyaniazi <farihadania@hotmail.com>
2023-07-15 22:54:33 +0500

    add : composite pattern prob

---
+++ Design Patterns/Structural/CompositePattern/src/Main.java
@@ -0,0 +1,22 @@
+import Problem.Group;
+import Problem.Shape;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        var group1 = new Group();
+        group1.add(new Shape());
+        group1.add(new Shape());
+    
+        var group2 = new Group();
+        group2.add(new Shape());
+        group2.add(new Shape());
+    
+    
+        var group = new Group();
+        group.add(group1);
+        group.add(group2);
+        // need to change the imp
+        group.render();
+    }
+}
