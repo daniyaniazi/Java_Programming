fa301d1b9b853170e2c2c05c7f2f25948ae477f2
daniyaniazi <farihadania@hotmail.com>
2023-07-18 21:23:04 +0500

    add : second question variety

--- ProblemSolving/Arrays/_03_Medium_RearrangeArrayBySign/src/Main.java
+++ ProblemSolving/Arrays/_03_Medium_RearrangeArrayBySign/src/Main.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+    // https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
     public int[] rearrangeArray(int[] nums) {
         int[] arr= new int[nums.length];
         int posInd=0;
@@ -15,5 +19,52 @@ class Solution {
             
         }
         return arr;
+    }
+}
+
+//https://www.codingninjas.com/studio/problems/alternate-numbers_6783445
+class SolutionNotEqualOccurences {
+    
+    public static int[] alternateNumbers(int[] a) {
+        // Write your code here.
+        List<Integer> pos = new ArrayList();
+        List<Integer> neg = new ArrayList();
+        // SC : O(N)
+        
+        for (int i : a) {
+            if (i < 0) {
+                neg.add(i);
+            } else {
+                pos.add(i);
+            }
+        }
+        
+        // TC : O(2N)
+        if (pos.size() > neg.size()) {
+            for (int i = 0; i < neg.size(); i++) {
+                a[2 * i] = pos.get(i);
+                a[(2 * i) + 1] = neg.get(i);
+            }
+            
+            int rem = neg.size() * 2;
+            for (int i = neg.size(); i < pos.size(); i++) {
+                a[rem] = pos.get(i);
+                rem++;
+            }
+        } else {
+            for (int i = 0; i < pos.size(); i++) {
+                a[2 * i] = pos.get(i);
+                a[(2 * i) + 1] = neg.get(i);
+            }
+            
+            int rem = pos.size() * 2;
+            for (int i = pos.size(); i < neg.size(); i++) {
+                a[rem] = neg.get(i);
+                rem++;
+            }
+            
+        }
+        
+        return a;
     }
 }
