db87e09b07ed03f14784abf5588e1ea326356155
daniyaniazii <daniya.niazi@techlogix.com>
2023-07-23 16:00:15 +0500

    add : set matrix zero optimal solution

--- ProblemSolving/Matrix/_01_Medium_SetMatrixZero/src/Main.java
+++ ProblemSolving/Matrix/_01_Medium_SetMatrixZero/src/Main.java
@@ -80,3 +80,55 @@ class Better {
     }
     
 }
+
+
+class Optimal {
+    public void setZeroes(int[][] matrix) {
+        
+        int col0=1;
+        // row  -> matrix[0][j] - to mark jth columns
+        // col --> matrix[i][0] - to mark ith row
+        for(int row=0;row<matrix.length;row++){
+            for(int col = 0 ; col<matrix[row].length;col++){
+                // M*N
+                if(matrix[row][col]==0){
+                    matrix[row][0] = 0; // mark Oth column to make ith row zero
+                    if(col!=0)
+                    {
+                        matrix[0][col] = 0 ; // mark 0th row at column jth to make column J zero
+                    }
+                    else{
+                        col0=0;
+                    }
+                    
+                }
+            }
+        }
+        
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    // check for col & row:
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j]= 0;
+                    }
+                }
+            }
+        }
+        
+        //step 3: Finally mark the 1st col & then 1st row:
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j]= 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+    }
+    
+}
