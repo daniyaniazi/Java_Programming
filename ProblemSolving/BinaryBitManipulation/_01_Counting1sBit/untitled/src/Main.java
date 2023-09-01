50a52e52d6d87b2e97e9290600e9e42a039ed9d2
daniyaniazi <farihadania@hotmail.com>
2023-09-01 23:29:57 +0500

    add : counting bits

---
+++ ProblemSolving/BinaryBitManipulation/_01_Counting1sBit/untitled/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] res= new int[n+1];
+        res[0] = 0;
+        
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                res[i] = res[i/2];
+            }
+            else{
+                res[i] = res[i/2]+1;
+            }
+        }
+        
+        return res;
+    }
+}
